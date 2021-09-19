24ade07dcac0965b8c57499ba342954319a72d0e
가재희 <jhka@gajaehuiui-MacBookPro.local>
2021-09-20 00:13:10 +0900

    Initial commit

---
+++ accountapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from accountapp.views import hello_world
+
+app_name = "accountapp"
+
+# 들어오고자 하는 경로: "127.0.0.1:8000/account/hello_world" <=> accountapp:hello_world
+
+urlpatterns = [
+    path('hello_world/', hello_world, name='hello_world')
+]
