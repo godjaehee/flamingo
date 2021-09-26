4cb19429c483651f7022ff48e2d6272a8dd10cfc
godjaehee <jhka@gajaehuiui-MacBookPro.local>
2021-09-26 18:13:39 +0900

    createview add

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 
 # 들어오고자 하는 경로: "127.0.0.1:8000/account/hello_world" <=> accountapp:hello_world
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
